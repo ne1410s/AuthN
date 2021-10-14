a3b98ba903b672c2ec851ccb0e4579a2cf45ba5f
ne1410s <pgj646@gmail.com>
2021-10-14 19:56:23 +0100

    Added test page

---
+++ AuthN.Api/Controllers/FacebookAuthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuthN.Api.Controllers
+{
+    /// <summary>
+    /// Controller for facebook authentication.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FacebookAuthController : ControllerBase
+    {
+    }
+}
